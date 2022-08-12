1ca59a4db104c5fe2611e60d75ac00f84293082a
Matsuri <nhungmaitran1412@gmail.com>
2022-08-12 21:02:59 +0700

    change duration of recording files

--- frontend/resources/Constants.py
+++ frontend/resources/Constants.py
@@ -4,8 +4,8 @@ json_filename = "data.json"
 
 audio_filepath = "../../extractAudio/wavs/voxceleb1/test/wav/"  # wav format file -> username/username/file.wav
 # audio_filepath = "../../extractAudio/wavs/voxceleb1/test/feat/test_logfbank_nfilt40"  # wav format file
-SIGNUP_DURATION = 3  # in seconds
-LOGIN_DURATION = 3  # in seconds
+SIGNUP_DURATION = 10  # in seconds
+LOGIN_DURATION = 10  # in seconds
 
 login_record_button_size = 310
 signup_record_button_size = 180
