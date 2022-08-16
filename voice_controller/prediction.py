e1a385fe24c95021dbd33f1cc4318ade016040d9
nhung <nhungmaitran1412@gmail.com>
2022-08-16 15:05:00 +0700

    folder controller model

--- voice_controller/prediction.py
+++ voice_controller/prediction.py
@@ -2,7 +2,7 @@ import librosa
 import tensorflow as tf
 import numpy as np
 
-SAVED_MODEL_PATH = "../model.h5"
+SAVED_MODEL_PATH = "model.h5"
 SAMPLES_TO_CONSIDER = 22050
 
 class _Keyword_Spotting_Service:
